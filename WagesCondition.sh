e2c036ac38ca31334988b9abfdf2ea9c9b8de7b6
ShravyaDevHub <70330349+ShravyaDevHub@users.noreply.github.com>
2020-08-29 21:57:36 +0530

    Update WagesCondition.sh

--- WagesCondition.sh
+++ WagesCondition.sh
@@ -6,17 +6,17 @@ while [ $hours -le 100 ] || [ $workingDays -le 20 ]
 do
 	attendance=$(( RANDOM%3 ))
 	case $attendance in
-   	0) dailyWage=$(( wagePerHour*0 ))
+   	0) totalWage=$(( wagePerHour*0 ))
 			(( workingDays++ ))
       	;;
-   	1) dailyWage=$(( wagePerHour*(hours/2) ))
+   	1) totalWage=$(( wagePerHour*(hours/2) ))
 			hours=$(( hours+4 ))
 			(( workingDays++ ))
       	;;
-   	*) dailyWage=$(( wagePerHour*hours ))
+   	*) totalWage=$(( wagePerHour*hours ))
 			hours=$(( hours+8 ))
 			(( workingDays++ ))
      		;;
 	esac
 done
-echo $dailyWage
+echo $totalWage
