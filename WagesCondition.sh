6227a8b4533f6923c8689eb63be9631a541d143d
unknown <shravyak614@gmail.com>
2020-08-29 21:50:06 +0530

    Added: WagesCondition.sh file

---
+++ WagesCondition.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+wagePerHour=20
+hours=8
+workingDays=20
+while [ $hours -le 100 ] || [ $workingDays -le 20 ]
+do
+	attendance=$(( RANDOM%3 ))
+	case $attendance in
+   	0) dailyWage=$(( wagePerHour*0 ))
+			(( workingDays++ ))
+      	;;
+   	1) dailyWage=$(( wagePerHour*(hours/2) ))
+			hours=$(( hours+4 ))
+			(( workingDays++ ))
+      	;;
+   	*) dailyWage=$(( wagePerHour*hours ))
+			hours=$(( hours+8 ))
+			(( workingDays++ ))
+     		;;
+	esac
+done
+echo $dailyWage
