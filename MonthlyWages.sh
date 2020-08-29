2e5fa364dc573b77b1f157318a7a0c46f5d03085
ShravyaDevHub <70330349+ShravyaDevHub@users.noreply.github.com>
2020-08-29 21:59:04 +0530

    Update MonthlyWages.sh

--- MonthlyWages.sh
+++ MonthlyWages.sh
@@ -4,11 +4,11 @@ hours=8
 workingDays=20
 attendance=$(( RANDOM%3 ))
 case $attendance in
-   0) dailyWage=$(( wagePerHour*0*workingDays ))
+   0) totalWage=$(( wagePerHour*0*workingDays ))
       ;;
-   1) dailyWage=$(( wagePerHour*(hours/2)*workingDays ))
+   1) totalWage=$(( wagePerHour*(hours/2)*workingDays ))
       ;;
-   *) dailyWage=$(( wagePerHour*hours*workingDays ))
+   *) totalWage=$(( wagePerHour*hours*workingDays ))
       ;;
 esac
-echo $dailyWage
+echo $totalWage
