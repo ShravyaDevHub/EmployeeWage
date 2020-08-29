a4b874712a672e81e18f48a380d01cb60e76997f
ShravyaDevHub <70330349+ShravyaDevHub@users.noreply.github.com>
2020-08-29 22:32:44 +0530

    Update WagesCondition.sh

--- WagesCondition.sh
+++ WagesCondition.sh
@@ -2,7 +2,7 @@
 wagePerHour=20
 hours=1
 workingDays=1
-while [ $hours -le 100 ] || [ $workingDays -le 20 ]
+while [ $hours -le 100 ] && [ $workingDays -le 20 ]
 do
 	attendance=$(( RANDOM%3 ))
 	case $attendance in
